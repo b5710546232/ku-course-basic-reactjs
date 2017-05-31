383147e3f4d7705569f5621f4919794576d07990
b5710546232 <nattapat.su@ku.th>
2017-05-31 10:34:30 +0700

    modify => cliker game

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-   let title = document.getElementById("title")
-        title.textContent = 'React for Engineer'
-
-        let message = document.getElementById("message")
-
-
-        const intervalTime = 1000
-        let time = 0
-        let interval = setInterval(() => {
-            time++
-            message.textContent = time + ' sec'
-        }, intervalTime)
-
-        let stopButton = document.getElementById("stop-button")
-
-        stopButton.addEventListener('click', () => {
-            clearInterval(interval)
-        })
+let clicks = 0
+let messageBox = document.getElementById('message')
+window.addEventListener('click',()=>{
+    clicks++
+    messageBox.textContent = clicks
+})
